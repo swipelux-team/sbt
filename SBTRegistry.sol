99733edfeaed0a138c3205a5987cb01fe3fe2bcf
Vladislav Simanov <42889004+rzalegal@users.noreply.github.com>
2023-07-12 15:05:28 +0300

    Add SBTRegistry.sol

---
+++ SBTRegistry.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {SBT} from "./SBT.sol";
+
+contract SBTRegistry is OwnableUpgradeable
+{
+    mapping(uint256 => mapping(address => uint256)) private permissions;
+    SBT public sbt;
+
+    constructor(address sbtContractAddress) OwnableUpgradeable() {
+        sbt = SBT(sbtContractAddress);
+    }
+
+    modifier hasSBT() {
+        require(currentTokenId() != uint256(0), "caller has no SBT issued");
+        _;
+    }
+
+    function grantPermission(address dAppAddress, uint256 level) external hasSBT {
+        permissions[currentTokenId()][dAppAddress] = level;
+        emit DappPemissionGranted(_msgSender(), currentTokenId(), dAppAddress, level);
+    }
+
+    function revokePermission(address dAppAddress) external hasSBT {
+        delete permissions[currentTokenId()][dAppAddress];
+        emit DappPemissionRevoked(_msgSender(), currentTokenId(), dAppAddress);
+    }
+
+    function isAllowed(address dAppAddress, address userAddress, uint256 level) external view returns(bool) {
+        uint256 tokenId = sbt.currentTokenId(userAddress);
+        require(tokenId != 0, "user has no SBT issued");
+        return permissions[tokenId][dAppAddress] >= level;
+    }
+
+    function currentTokenId() internal view returns (uint256) {
+        return sbt.currentTokenId(_msgSender());
+    }
+
+    event DappPemissionRevoked(address user, uint256 tokenId, address dAppAddress);
+    event DappPemissionGranted(address user, uint256 tokenId, address dAppAddress, uint256 level);
+}
